3f71ed686486f8c87bb58f30a1b6d47dd8f24453
Liam Ross <liamross@telus.net>
2017-08-15 20:43:17 -0700

    added license header

--- src/Resizer/Resizer.jsx
+++ src/Resizer/Resizer.jsx
@@ -1,3 +1,11 @@
+/* Resizer.jsx -- A wrapper for resizing content to hit a pre-defined interval.
+ *
+ * Copyright (C) 2017 Liam Ross
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
