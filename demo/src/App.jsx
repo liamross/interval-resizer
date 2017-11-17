3585d81c5657d3101d7a3e587b3691103593691f
Liam Ross <liamross@telus.net>
2017-11-16 22:14:45 -0800

    new demo

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import IntervalResizer from '../../src/IntervalResizer';
+import IntervalResizer from './IntervalResizer';
 import './App.css';
 
 class App extends Component {
